7ce92fa809c1f18a44820fecb93a9aa35d1a8f8f
sophie <sophie.ischenko@icloud.com>
2023-11-27 16:33:59 +0100

    greeting

---
+++ greetingApp.js
@@ -0,0 +1,5 @@
+function submitName() {
+    const enteredName = document.getElementById('nameInput').value;
+    const greetingMessage = `Hello, ${enteredName}!`;
+    document.getElementById('greetingMessage').innerText = greetingMessage;
+  }
