823518d29ab447901563a882185ea5cfd862c0c0
Sophie Ischenko <82713289+Sophies-Creative-hub@users.noreply.github.com>
2023-11-28 11:12:02 +0100

    Update main.js

--- frontend/main.js
+++ frontend/main.js
@@ -1,7 +1,11 @@
 function submitName() {
   const enteredName = document.getElementById('nameInput').value;
   const greetingMessage = `Hello, ${enteredName}!`;
-  document.getElementById('greetingMessage').innerText = greetingMessage;
+  updateUI(greetingMessage);
 }
 
-module.exports = { submitName };
+function updateUI(message) {
+  document.getElementById('greetingMessage').innerText = message;
+}
+
+module.exports = { submitName, updateUI };
